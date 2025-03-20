05bfd43af7424d0d3a85e2cadcab19ea2c27363e
99JIWONDEV <99jiwonpark@gmail.com>
2025-03-20 15:32:37 +0900

    fix: footer height 수정

--- src/containers/cart/CartContainer.style.js
+++ src/containers/cart/CartContainer.style.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const StyledCartContainer = styled.div `
   max-width: 1200px;
   width: 100vw;
-  height: 100vh;
   margin: 75px auto;
   padding-top: 20px;
   display: flex;
   justify-content: center;
+  min-height: 100vh;
 `;
