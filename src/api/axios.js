fd0e270463b98d48a72ea628e5e48e125278b001
khwww <khw4420@knu.ac.kr>
2025-03-20 12:26:17 +0900

    feat: 구현기능 재조정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080', // API 기본 URL
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
---
+++ src/api/register.js
@@ -0,0 +1,26 @@
+import API from './axios';
+
+// 회원가입 요청 함수
+export const registerUser = async (formData) => {
+  try {
+    const response = await API.post('/member/signup', {
+      email: `${formData.email}@${formData.emailSelect}`,
+      pwd: formData.password,
+      nickname: formData.nickname,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : '회원가입 요청 실패';
+  }
+};
+
+// 이메일 중복 확인 요청 함수
+export const checkEmailAvailability = async (email) => {
+  try {
+    const response = await API.post('/member/check-email', { email });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || '이메일 중복 확인 실패';
+  }
+};
